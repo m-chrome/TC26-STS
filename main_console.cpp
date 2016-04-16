68bcf8e3cb0cdb9b37f507346e69b1294b0e4d3f
Mikhail Tsyganov <tsyganov.michail@yandex.ru>
2016-04-16 16:33:30 +0300

    Вывод результатов на экран и в файл.

--- main_console.cpp
+++ main_console.cpp
@@ -3,16 +3,17 @@
 
 #include <cstring>
 
-#include <QCoreApplication>
-#include <qglobal.h>
-
 int main(int argc, char *argv[])
 {
     Core core;
     if(!strcmp(argv[1],"--help"))
     {
-        Console::help(core);
+        Console::Help(core);
         return 0;
+    }
+    if(core.m_isAllChecked == 1)
+    {
+        Console::OutputResults(core);
     }
     return 0;
 }
