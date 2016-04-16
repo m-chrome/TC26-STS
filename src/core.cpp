f5296e973e34a855e0f5762ccb26ea4f64e047b2
Mikhail Tsyganov <tsyganov.michail@yandex.ru>
2016-04-16 16:31:47 +0300

    Функция парсинга для тестов и решений; начаты конструктор и деструктор.

--- src/core.cpp
+++ src/core.cpp
@@ -13,14 +13,22 @@
 
 Core::Core()
 {
-
+    m_isAllChecked = 0;
+    ParsingTests("~/libs/statistical_tests");
+    ParsingDecisions("~/libs/decisions");
+    // TODO: функция для выбора файла для тестирования
+    // TODO: функция для заполнения выбранных тестов
+    // TODO: функция для заполнения выбранных решений
+    Logic();
 }
 
 Core::~Core()
 {
     m_openFiles.clear();
-    m_tests.clear();
-    m_decisions.clear();
+    m_useTests.clear();
+    m_useDecisions.clear();
+    m_allTests.clear();
+    m_allDecisions.clear();
 }
 
 void Core::Logic()
@@ -29,7 +37,7 @@ void Core::Logic()
     for(auto& currentStream: m_openFiles)
     {
         // Идём по выбранным тестам
-        for(auto& currentTest: m_tests)
+        for(auto& currentTest: m_useTests)
         {
             std::vector<double> CurPvalues;
             while(currentStream.first)
@@ -45,26 +53,46 @@ void Core::Logic()
             m_pvalues.push_back(CurPvalues);
 
             // Идём по модулям принятия решений
-            for(auto& currentDecision: m_decisions)
+            for(auto& currentDecision: m_useDecisions)
             {
-                if ((*currentDecision.m_func)(CurPvalues, currentDecision.m_alfa))
-                    std::cout << "success";
-                else
+                if ((*currentDecision.m_func)(CurPvalues, currentDecision.m_alpha))
+                    continue;
+                else  
                 {
-                    std::cout << "fail";
-                    currentStream.second=0;
+                    (currentStream.second).testResult=0;
+                    break;
                 }
             }
         }
+
         // Всё хорошо, все тесты выполнены и поток успешно
         // прошёл через тесты модуля принятия решения
-        currentStream.second=1;
+        (currentStream.second).testResult=1;
+    }
+    m_isAllChecked = 1;
+}
+
+void Core::ParsingTests(QString dest)
+{
+    QDir dir(dest);
+    QStringList filters;
+    filters << "*.so" << "*.dll" << "*.DLL";
+    QStringList plugins=dir.entryList(filters);
+    for(auto& el:plugins)
+    {
+        QString plugin=el.right(el.size()-3);   //обрезка lib в названии
+        plugin=plugin.section('.',0,0);         //обрезка расширения
+        QLibrary lib(plugin);
+        if (!lib.load()) std::cout << lib.isLoaded() << ' ' << lib.errorString().toStdString() << '\n';
+        tc26::TestFunc_t* fct = new tc26::TestFunc_t ((tc26::TestFunc_t)(lib.resolve(plugin.toStdString().c_str())));
+        if(!fct) std::cout << lib.errorString().toStdString() << '\n';
+        m_allTests.emplace_back(tc26::TestObj(*fct,(std::pair<int,char**>){0,NULL},plugin.toStdString()));
     }
 }
 
-void Core::Parsing()
+void Core::ParsingDecisions(QString dest)
 {
-    QDir dir("~/libs/statistical tests");
+    QDir dir(dest);
     QStringList filters;
     filters << "*.so" << "*.dll" << "*.DLL";
     QStringList plugins=dir.entryList(filters);
@@ -74,8 +102,9 @@ void Core::Parsing()
         plugin=plugin.section('.',0,0);//обрезка расширения
         QLibrary lib(plugin);
         if (!lib.load()) std::cout << lib.isLoaded() << ' ' << lib.errorString().toStdString() << '\n';
-        tc26::TestFunc_t* fct = new tc26::TestFunc_t ((tc26::TestFunc_t)(lib.resolve(plugin.toStdString().c_str())));
+
+        tc26::DeсisionFunc_t* fct = new tc26::DeсisionFunc_t((tc26::DeсisionFunc_t)(lib.resolve(plugin.toStdString().c_str())));
         if(!fct) std::cout << lib.errorString().toStdString() << '\n';
-        m_allTests.emplace_back(tc26::TestObj(*fct,(std::pair<int,char**>){0,NULL},plugin.toStdString()));
+        m_allDecisions.emplace_back(tc26::DecisionObj(*fct, 0,(std::pair<int,char**>){0,NULL},plugin.toStdString()));
     }
 }
