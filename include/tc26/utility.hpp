c3d7391b509806b92c3984255cb13e825555c7b0
Mikhail Tsyganov <tsyganov.michail@yandex.ru>
2016-04-16 16:38:31 +0300

    Красоту навёл.

--- include/tc26/utility.hpp
+++ include/tc26/utility.hpp
@@ -10,10 +10,14 @@ struct testInfo
     std::string description;
 };
 
-std::map<std::string,testInfo> utilityTable{
-    {"monobittest",{0,"no arguments"}},{"runtest",{0,"no arguments"}},{"ovrlpchisquaretest",{1,"[a1] is a length of a tuple"}},
+std::map<std::string,testInfo> utilityTable
+{
+    {"monobittest",{0,"no arguments"}},
+    {"runtest",{0,"no arguments"}},
+    {"ovrlpchisquaretest",{1,"[a1] is a length of a tuple"}},
     {"gaptest",{4,"[a1-1] is maximum length of the interval; [a2] is a length of a tuple; [a3] is a left border of the testing section; [a4] is a right border of the testing section"}},
-    {"maurertest",{0,"no arguments"}}, {"randomwalktest",{0,"no arguments"}}
+    {"maurertest",{0,"no arguments"}},
+    {"randomwalktest",{0,"no arguments"}}
 };
 #endif // UTILITY_HPP
 
