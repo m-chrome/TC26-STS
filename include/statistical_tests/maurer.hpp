3a097cf31872937c61455df6cc08a0287d6da127
Mikhail Tsyganov <tsyganov.michail@yandex.ru>
2016-04-17 14:12:19 +0300

    Заготовка для Maurer Test.

---
+++ include/statistical_tests/maurer.hpp
@@ -0,0 +1,8 @@
+#ifndef MAURER_HPP
+#define MAURER_HPP
+
+#include <fstream>
+
+double maurertest(std::ifstream &inputFile, int argc, char* argv[]);
+
+#endif
---
+++ src/maurer.cpp
@@ -0,0 +1,7 @@
+#include "include/statistical_tests/maurer.hpp"
+
+double maurertest(std::ifstream &inputFile, int argc, char* argv[])
+{
+    double pvalue=0;
+    return pvalue;
+}
