5421ff1841da2d29f1d88cb5c168516f12d3634e
Anna Shukaleva <cygnus@michiru.ru>
2016-05-29 14:54:01 +0300

    randomwalk test, header

---
+++ include/statistical_tests/randomwalk.hpp
@@ -0,0 +1,22 @@
+#ifndef RANDOMWALK_HPP
+#define RANDOMWALK_HPP
+
+#include <fstream>
+
+#ifdef WIN32
+#ifdef FEX_CNBCDLL
+#define DLLEXPORT __declspec(dllexport)
+#else
+#define DLLEXPORT __declspec(dllimport)
+#endif
+#else
+#define DLLEXPORT
+#endif
+
+
+extern "C" DLLEXPORT
+{
+    double randomwalktest(std::ifstream &input, int argc, char* argv[]);
+}
+
+#endif
